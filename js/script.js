488d1154c1dd3fc929856be890f9861424d3a3b5
Tanvir Aslam <tanviraslam@outlook.com>
2014-05-14 23:52:09 -0700

    Animated background, added win and lose screens

--- js/script.js
+++ js/script.js
@@ -7,8 +7,6 @@ $(document).ready(function(){
 		sound.play();
 	});
 	
-	
-
 	//Random lightsaber based hover colors on answers
 	$(document.body).on('mouseenter', '.answers ul li', function(){
 		var colors = ["#00CDCD", "#EE7600", "#FF4500", "#FFD700"];
@@ -17,7 +15,6 @@ $(document).ready(function(){
 		$(this).css('background', 'rgba(0,0,0,0)');
 	}).on('click', '.answers ul li', function(){
 		checkAnswer($(this));
-		//loadQuestion();
 	});
 });
 
@@ -37,8 +34,6 @@ function getQuestions(){
 
 //Called to load the first question when the quiz is started.
 function loadQuestion(){
-	//Make keys a global variable and load it up when the questions are loaded the first time.
-	
 	var key = quiz.keys[Math.floor(Math.random()*quiz.keys.length)];
 	quiz.curAnswer = quiz.questionSet[key]['a'];
 	var qhtml = "<div id='" + key + "'><p>Q: "+quiz.questionSet[key]["q"]+"</p><div class='answers'><ul><li>" + quiz.questionSet[key]["op1"] + "</li><li>" + quiz.questionSet[key]["op2"] + "</li><li>" + quiz.questionSet[key]["op3"] + "</li><li>" + quiz.questionSet[key]["op4"] + "</li></ul></div></div>" ;
@@ -51,37 +46,39 @@ function loadQuestion(){
 }   
 
 function checkAnswer(answer){
-	console.log(answer.text());
-	if(answer.text() === quiz.curAnswer){
-		var sound = $('#yoda-' + Math.ceil(Math.random()*3))[0];
-		sound.load();
-		sound.play();
-		answer.css("background","green");
-		$('#saber').animate({"height":140*quiz.correct + "px"},800).show();
-		quiz.correct++;
-	}else{
-		var sound = $('#darkside-' + Math.ceil(Math.random()*6))[0];
-		sound.load();
-		sound.play();
-		answer.css("background","red");
-		$('#saber-dark').animate({"height":140*quiz.wrong + "px"},800).show();
-		quiz.wrong++;
-	}
+	var answerLimit = 5;
+	if(quiz.correct !== answerLimit && quiz.wrong !== answerLimit){
+		if(answer.text() === quiz.curAnswer){
+			var sound = $('#yoda-' + Math.ceil(Math.random()*3))[0];
+			sound.load();
+			sound.play();
+			answer.css("background","green");
+			$('#saber').animate({"height":140*quiz.correct + "px"},800).show();
+			quiz.correct++;
+		}else{
+			var sound = $('#darkside-' + Math.ceil(Math.random()*6))[0];
+			sound.load();
+			sound.play();
+			answer.css("background","red");
+			$('#saber-dark').animate({"height":140*quiz.wrong + "px"},800).show();
+			quiz.wrong++;
+		}
 
-	quiz.curAnswer = "";
-	setTimeout(function(){loadQuestion();}, 800);
+		quiz.curAnswer = "";
+		setTimeout(function(){loadQuestion();}, 800);
+	}else{
+		if(quiz.correct === answerLimit) winScreen();
+		else loseScreen();
+	}
 }
 
 function initialize(){
 	quiz.correct = 1;
 	quiz.wrong = 1;
-	$('#questions').css("visibility","hidden");
-	$('#progress-saber').hide();
-	$('#progress-saber-dark').hide();
+	hideMainScreen();
 	
 	$('#start').click(function(){
-		console.log("test");
-		$(this).hide();
+		$(this).animate({"-webkit-transform":"scale(2)"}, 800);
 		$("#questions").css('visibility', "visible");
 		$('#progress-saber,#progress-saber-dark').show();
 		// $('#saber,#saber-dark').hide();
@@ -91,5 +88,21 @@ function initialize(){
 	//Load the first question
 	getQuestions();
 	loadQuestion();
+}
+
+function winScreen(){
+	hideMainScreen();
+	$('#win').show();
+}
+
+function loseScreen(){
+	hideMainScreen();
+	$('#lose').show();
+}
+
+function hideMainScreen(){
+	$('#questions').css("visibility","hidden");
+	$('#progress-saber').hide();
+	$('#progress-saber-dark').hide();
 }
                                                                                                                                                                                                                                                                                                        
